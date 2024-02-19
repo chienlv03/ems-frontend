339d6d801a2f56dd32db84c4c09c987125461e8a
chienlv03 <levanchienhd03@gmail.com>
2024-02-19 09:39:28 +0700

    Add new project ems-frontend

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+                <p>All rights reserved 2024 by Chien</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
